f0ab567b5e8a330200d517f017e03763828c38c0
rabm2k7 <rabmiller1@gmail.com>
2016-11-26 18:42:15 +0100

    commit2

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,10 +1,19 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope, $cordovaGeolocation) {
+.controller('DashCtrl', function($scope, $cordovaGeolocation, $http) {
+
+  $scope.dataAvailable = [];
 
   var options = {timeout: 10000, enableHighAccuracy: true};
 
     $cordovaGeolocation.getCurrentPosition(options).then(function(position){
+
+    $http.get("airbnb.json")
+      .then(function(response) {
+
+      $scope.geoloc = response.data;
+
+      console.log($scope.geoloc);
 
       var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
@@ -16,30 +25,19 @@ angular.module('starter.controllers', [])
 
       $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
+      //for (var i=0,tot=$scope.dataAvailable.length;i<tot; i++) {
+                  var marker = new google.maps.Marker({
+                map: $scope.map,
+                animation: google.maps.Animation.DROP,
+                position: latLng
+            });
+      //}
+
     }, function(error){
       console.log("Could not get location");
     });
 
-    //Wait until the map is loaded
-
-    google.maps.event.addListenerOnce($scope.map, 'idle', function(){
-
-    var marker = new google.maps.Marker({
-        map: $scope.map,
-        animation: google.maps.Animation.DROP,
-        position: latLng
-    });
-
-    var infoWindow = new google.maps.InfoWindow({
-        content: "Here I am!"
-    });
-
-    google.maps.event.addListener(marker, 'click', function () {
-        infoWindow.open($scope.map, marker);
-    });
-
-    });
-
+  });
 
 })
 
