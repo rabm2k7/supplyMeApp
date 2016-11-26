1ab7f7d6b2bd4885d9a0857444784f937baa1c96
rabm2k7 <rabmiller1@gmail.com>
2016-11-26 16:48:32 +0100

    init comit

---
+++ www/js/controllers.js
@@ -0,0 +1,69 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope, $cordovaGeolocation) {
+
+  var options = {timeout: 10000, enableHighAccuracy: true};
+
+    $cordovaGeolocation.getCurrentPosition(options).then(function(position){
+
+      var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+      var mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+
+      $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+    }, function(error){
+      console.log("Could not get location");
+    });
+
+    //Wait until the map is loaded
+
+    google.maps.event.addListenerOnce($scope.map, 'idle', function(){
+
+    var marker = new google.maps.Marker({
+        map: $scope.map,
+        animation: google.maps.Animation.DROP,
+        position: latLng
+    });
+
+    var infoWindow = new google.maps.InfoWindow({
+        content: "Here I am!"
+    });
+
+    google.maps.event.addListener(marker, 'click', function () {
+        infoWindow.open($scope.map, marker);
+    });
+
+    });
+
+
+})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
